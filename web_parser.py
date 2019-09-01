dda67870ae460d53811bb613278df19c6a28aabc
miyanishi <miyanishi_kazunori@cyberagent.co.jp>
2019-09-01 16:00:38 +0900

    add_download_detail_page

--- web_parser.py
+++ web_parser.py
@@ -2,9 +2,19 @@
 from urllib import request
 from bs4 import BeautifulSoup
 import os
+import urllib.parse
+import requests
 
 
-def parse_year_page(year_url):
+def download_inning_data(gid_url, out_path):
+    inning_url = os.path.join(gid_url, "inning/inning_all.xml")
+    # urllib.parse.urljoinだと、末尾からファイル名と判定すると（/で終わっていない場合）、ファイル名を除外して結合してしまうので、、
+    print(inning_url)
+    response = requests.get(inning_url)
+    with open(out_path, 'wb') as file:
+        file.write(response.content)
+
+def parse_year_page(year_url, out_dir):
     for m in range(3, 12):
         for d in range(1, 32):
             # print("month_{:02}/day_{:02}".format(m, d))
@@ -12,10 +22,17 @@ def parse_year_page(year_url):
             print("path: {}".format(parse_path))
             html = request.urlopen(parse_path)
             soup = BeautifulSoup(html, 'html.parser')
-            for link in (soup.find_all("li")):
-                print(link)
+            link_string_list = [link.string.strip() for link in (soup.find_all("li"))]
+            for link in link_string_list:
+                if link.startswith("gid"):
+                    gid_url = os.path.join(parse_path, link)
+                    gid = link.replace("/", "")
+                    out_path = os.path.join(out_dir, gid+".xml")
+                    download_inning_data(gid_url, out_path)
+                    exit(0)
             exit(0)
 
 if __name__ == '__main__':
     year_url = "http://gd2.mlb.com/components/game/mlb/year_2018"
-    parse_year_page(year_url)
+    out_dir = "data"
+    parse_year_page(year_url, out_dir)
